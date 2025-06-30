89e93c2afd46868812e6ffafa9ba6d1ff294a479
GEORGE-SCUTARU <88233902+GEORGE-SCUTARU@users.noreply.github.com>
2025-06-30 22:18:36 +0100

    update

--- GOLD.service.worker.js
+++ GOLD.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751314252|1653563115';
+const CACHE_VERSION = '1751318305|5706412051';
 /** @type {string} */
 const CACHE_PREFIX = 'GOLD-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
