d99c6d60d7947898281ecdd739ccbd51b02a449f
GEORGE-SCUTARU <88233902+GEORGE-SCUTARU@users.noreply.github.com>
2025-08-26 16:50:25 +0100

    dgdfgdfgdfg

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756073354|17584854679';
+const CACHE_VERSION = '1756223414|2585578457';
 /** @type {string} */
 const CACHE_PREFIX = 'GOLD QUEST-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
